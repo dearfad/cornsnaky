b8c044cc3304cef6ee458c65a5791d84752129b8
dearfad <dearfad@gmail.com>
2025-01-03 10:48:27 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <v-container class="pa-12">
     <v-row>
       <v-col cols="12" md="3" class="mx-auto">
+        hun
         <v-btn size="large" block to="/login">请访问登录页</v-btn>
       </v-col>
     </v-row>
